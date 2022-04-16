LineageOS/android_device_xiaomi_Mi8937

hw-qcom-camera: Rename properties with vendor prefix                 according to new sepolicy rules.
Change-Id: Icde3e239692d645c610328bd9997de7b338d6ef8

QCamera2/stack/mm-camera-interface/src/mm_camera_interface.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2016, 2019, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -1686,7 +1686,7 @@ void sort_camera_info(int num_cam)
 
     // Signifies whether YUV AUX camera has to be exposed as physical camera
     memset(prop, 0, sizeof(prop));
-    property_get("persist.camera.aux.yuv", prop, "0");
+    property_get("persist.vendor.camera.aux.yuv", prop, "0");
     is_yuv_aux_cam_exposed = atoi(prop);
     LOGI("YUV Aux camera exposed %d",is_yuv_aux_cam_exposed);
 
@@ -1723,7 +1723,7 @@ void sort_camera_info(int num_cam)
         }
     }
 
-    /* Expose YUV AUX camera if persist.camera.aux.yuv is set to 1.
+    /* Expose YUV AUX camera if persist.vendor.camera.aux.yuv is set to 1.
     Otherwsie expose AUX camera if it is not YUV. */
     for (i = 0; i < num_cam; i++) {
         if ((g_cam_ctrl.info[i].facing == CAMERA_FACING_BACK) &&
@@ -1739,7 +1739,7 @@ void sort_camera_info(int num_cam)
         }
     }
 
-    /* Expose YUV AUX camera if persist.camera.aux.yuv is set to 1.
+    /* Expose YUV AUX camera if persist.vendor.camera.aux.yuv is set to 1.
     Otherwsie expose AUX camera if it is not YUV. */
     for (i = 0; i < num_cam; i++) {
         if ((g_cam_ctrl.info[i].facing == CAMERA_FACING_FRONT) &&

Mi-Thorium/android_device_xiaomi_land-camera

camera/QCamera2/stack/mm-camera-interface/src/mm_camera_interface.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2016, 2019, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -1596,12 +1596,12 @@ void sort_camera_info(int num_cam)
 
     // Signifies whether system has to enable dual camera mode
     memset(prop, 0, sizeof(prop));
-    property_get("persist.camera.dual.camera", prop, "0");
+    property_get("persist.vendor.camera.dual.camera", prop, "0");
     is_dual_cam = atoi(prop);
 
     // Signifies whether AUX camera has to be exposed as physical camera
     memset(prop, 0, sizeof(prop));
-    property_get("persist.camera.aux.camera", prop, "0");
+    property_get("persist.vendor.camera.aux.camera", prop, "0");
     is_aux_cam_exposed = atoi(prop);
     LOGI("dualCamera:%d auxCamera %d",
             is_dual_cam, is_aux_cam_exposed);